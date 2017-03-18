948b4cef755156e121d4fa9ffb46a5ec6a571060
Kürşat <kvurusan@gmail.com>
2017-03-18 23:21:50 +0300

    Adding files

---
+++ Twitter Username Chk/Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RichTextBox3 = New System.Windows.Forms.RichTextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(24, 461)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(131, 56)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'BackgroundWorker1
+        '
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(13, 13)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(184, 284)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 334)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(97, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Current: None"
+        '
+        'RichTextBox3
+        '
+        Me.RichTextBox3.Location = New System.Drawing.Point(375, 12)
+        Me.RichTextBox3.Name = "RichTextBox3"
+        Me.RichTextBox3.Size = New System.Drawing.Size(234, 215)
+        Me.RichTextBox3.TabIndex = 4
+        Me.RichTextBox3.Text = ""
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(13, 304)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Open"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(10, 380)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(706, 23)
+        Me.ProgressBar1.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(345, 360)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(28, 17)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "0%"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(728, 529)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.RichTextBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RichTextBox3 As RichTextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Twitter Username Chk/Form1.vb
@@ -0,0 +1,79 @@
+﻿Imports System.IO
+Imports System.Net
+
+Public Class Form1
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        For i = 1 To RichTextBox1.Lines.Length
+            Label1.Text = "Current: " & RichTextBox1.Lines(i - 1) & " " & i & "/" & RichTextBox1.Lines.Length
+            ProgressBar1.Value = i
+            istek(RichTextBox1.Lines(i - 1))
+            Label2.Text = String.Format("{0}%", ((ProgressBar1.Value / ProgressBar1.Maximum) * 100).ToString("F2"))
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        CheckForIllegalCrossThreadCalls = False
+        ProgressBar1.Maximum = RichTextBox1.Lines.Length
+        BackgroundWorker1.RunWorkerAsync()
+
+    End Sub
+    Public Function istek(ByVal nickname As String)
+        Try
+            Dim request As WebRequest =
+             WebRequest.Create("https://mobile.twitter.com/" & nickname)
+
+            ' If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials
+            ' Get the response.
+            Dim response As WebResponse = request.GetResponse
+            '' Display the status.
+
+            '' Get the stream containing content returned by the server.
+            'Dim dataStream As Stream = response.GetResponseStream()
+            '' Open the stream using a StreamReader for easy access.
+            'Dim reader As New StreamReader(dataStream)
+            '' Read the content.
+            'Dim responseFromServer As String = reader.ReadToEnd()
+            '' Display the content.
+            '' RichTextBox2.Text += responseFromServer & vbCrLf
+
+            'Dim regex As New Text.RegularExpressions.Regex("<\s*title[^>]*>(.*?)<\s*/\s*title>")
+            'Dim results As New Text.StringBuilder
+            'For Each m As Text.RegularExpressions.Match In regex.Matches(responseFromServer)
+            '    results.Append(m.Value)
+            'Next
+            'If InStr(results.ToString, "on Twitter") = False Then
+            '    If InStr(responseFromServer, "This account has been suspended.") = False Then
+            '        RichTextBox3.Text += nickname & vbCrLf
+            '    End If
+            'End If
+
+            ' Clean up the streams and the response.
+            'reader.Close()
+            response.Close()
+        Catch ex As Exception
+            RichTextBox3.Text += nickname & vbCrLf
+        End Try
+
+
+        Return True
+
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim openFile1 As New OpenFileDialog()
+
+        ' Initialize the OpenFileDialog to look for RTF files.
+        openFile1.DefaultExt = "*.txt"
+        openFile1.Filter = "TXT Files|*.txt"
+
+        ' Determine whether the user selected a file from the OpenFileDialog.
+        If (openFile1.ShowDialog() = System.Windows.Forms.DialogResult.OK) _
+            And (openFile1.FileName.Length > 0) Then
+
+            ' Load the contents of the file into the RichTextBox.
+            RichTextBox1.LoadFile(openFile1.FileName, RichTextBoxStreamType.PlainText)
+        End If
+
+    End Sub
+End Class
